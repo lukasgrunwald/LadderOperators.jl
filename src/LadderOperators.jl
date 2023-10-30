7086ff3a3a34bc045f26ce2ced840ccc245b9e0f
Lukas Grunwald <lukas.grunwald@rwth-aachen.de>
2023-10-30 15:42:55 +0100

    Add multi site spin-operators (with Unit Tests) Use extras for test dependencies instead seperate Project.toml

--- src/LadderOperators.jl
+++ src/LadderOperators.jl
@@ -1,8 +1,9 @@
 module LadderOperators
 
 # Matrix rep of Ladder operators
-export local_spinless_fermions, local_fermions, local_fermions_multiorbital, local_bosons
-export ladder_operators
+export local_spinless_fermions, local_fermions, local_fermions_multiorbital
+export local_bosons, local_spin
+export ladder_operators, spin_operators
 
 # Basis transformations
 export embed_fock
@@ -21,11 +22,20 @@ include("show_basis.jl")
 """
     ladder_operators(type, Nsites, args...)
 
-Matrix representation of Ladder operator of full antisymmetric Fock-space. Options are
-{:spinless, :fermi, :fermi_multi, :boson}. The return operator will have shape (Nsites, Ninternal...)
+Generate matrix represantion of ladder operators for lattice models in full (anti-) symmetric
+Fock space based on the specified type and system size.
+
+# Arguments
+- `type{:spinless, :fermi, :fermi_multi, :bosons}`: The type of ladder operators to generate.
+- `Nsites::Int`: The number of sites in the system.
+- `args...`: Additional arguments required for specific operator types.
+
+# Returns
+- A tuple containing two matrices: `Cdag` and `C`. These matrices represent the creation and
+annihilation operators for the specified ladder operators and have shape (Nsites, Ninternal...)
 """
 function ladder_operators(type, Nsites, args...)
-    # Load the appropriate opeartors
+    # Load the appropriate local opeartors
     if type === :spinless
         Cdag, C, Z, _ = local_spinless_fermions()
     elseif type === :fermi
@@ -45,6 +55,29 @@ function ladder_operators(type, Nsites, args...)
     C = embed_fock(Nsites, C, Z)
 
     return Cdag, C
+end
+"""
+    spin_operators(type, Nsites, args...)
+
+Generate multi-site spin operators based on the specified type and system size.
+
+# Arguments
+- `type{:heisenberg, :fermi}`: The type of spin representation to generate.
+- `Nsites::Int`: The number of sites or particles in the system.
+- `args...`: Additional arguments, if needed.
+
+# Returns
+- Array containing multi-site spin operators. The dimensions are (Nsites, S_{i=x,y,z}).
+"""
+function spin_operators(type, Nsites, args...)
+    # Generate site-local operators
+    s⃗ = local_spin(type) # Spin-1/2 operator
+    dim = size(s⃗[1], 1)
+    Z = I(dim) # No Wigner strings for spin-operators
+
+    S⃗ = embed_fock(Nsites, s⃗, Z) # Embed into multi-particle fock space
+
+    return S⃗
 end
 
 end # module
